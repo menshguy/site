a88cd58b6f1977f4df8a75a07c043d7fbf434446
Jeff Fenster <fenster.js@gmail.com>
2024-12-19 15:28:51 -0500

    removing resize event for now

--- src/context/DeviceContext.tsx
+++ src/context/DeviceContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
 
 interface DeviceContextProps {
   deviceWidth: number;
@@ -9,24 +9,24 @@ interface DeviceContextProps {
 const DeviceContext = createContext<DeviceContextProps | undefined>(undefined);
 
 export const DeviceProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [deviceInfo, setDeviceInfo] = useState<DeviceContextProps>({
+  const [deviceInfo, _setDeviceInfo] = useState<DeviceContextProps>({
     deviceWidth: window.innerWidth,
     deviceHeight: window.innerHeight,
     isMobile: window.innerWidth <= 768,
   });
 
-  useEffect(() => {
-    const handleResize = () => {
-      setDeviceInfo({
-        deviceWidth: window.innerWidth,
-        deviceHeight: window.innerHeight,
-        isMobile: window.innerWidth <= 768,
-      });
-    };
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     setDeviceInfo({
+  //       deviceWidth: window.innerWidth,
+  //       deviceHeight: window.innerHeight,
+  //       isMobile: window.innerWidth <= 768,
+  //     });
+  //   };
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
 
   return (
     <DeviceContext.Provider value={deviceInfo}>
