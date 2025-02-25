1350c1de0e1e750d20a850e5ae5621f5cf4ab8c6
Jeff Fenster <fenster.js@gmail.com>
2025-02-25 13:39:39 -0500

    vercel config fix, improvements to RAG documents

--- api/index.py
+++ api/index.py
@@ -2,10 +2,11 @@ import os
 import requests
 from dotenv import load_dotenv
 from openai import OpenAI
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import glob
 import json
+
 app = Flask(__name__)
 
 load_dotenv(override=True)
@@ -38,7 +39,7 @@ system_prompt = "You are a chatbot acting on behalf of me. All of your messages 
 from the user about me using the context I will provide below."
 system_prompt += "\n Relay any relevant links from my personal site(s) to the user."
 system_prompt += "\n Your responses should be in casual language as if texting a friend (for example try to use contractions like can't instead of cannot. Use common text language like lol, haha, etc.). Do not make any jokes. Do not make anything up if you haven't been provided with relevant context."
-system_prompt += "\n It is important that you answer any questions using the context provided below."
+system_prompt += "\n It is important that you answer any questions by using the context provided below."
 system_prompt += "\n If you cannot find a suitable answer in the provided context, respond by indicating you are not sure how to answer. Do not reveal that you are a bot or ai."
 system_prompt += "\n\n Below is the context about me (Jeff Fenster) that you must use to answer all questions: "
 system_prompt += "\n <Context>"
@@ -48,6 +49,7 @@ system_prompt += "\n</Context>"
 print("RAG CONTEXT:", system_prompt)
 
 def user_prompt_for(user_input):
+  print("USER INPUT: " + user_input)
   user_prompt = f"You have received the following question from the user about me: {user_input}. Answer the question."
   return user_prompt
 
@@ -65,13 +67,30 @@ def bot_response(user_input):
   )
   return response.choices[0].message.content
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 @app.route('/api/bot', methods=['GET'])
 def response():
     default_response = 'Tell me about Jeff' # default message if none provided
-    user_message = request.args.get('message', default_response)  
+    user_message = request.args.get('message', default_response)
+    print("USER MESSAGE FROM FE: " + user_message)
     response = bot_response(user_message)
     return jsonify({"message": response})
 
+@app.route('/api/healthcheck', methods=['GET'])
+def healthcheck():
+    return jsonify({"status": "healthy"})
 
+app.debug = True
+
+# Vercel serverless handler
 if __name__ == '__main__':
-  app.run(port=5328)
+    app.run(host='0.0.0.0', port=3000)
+
+# This is the handler that Vercel will use
+app = app
