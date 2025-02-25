3504d47f5acdb116223f9c15cb2bf5b369587e42
Jeff Fenster <fenster.js@gmail.com>
2025-02-25 12:37:35 -0500

    prompt update

--- api/index.py
+++ api/index.py
@@ -35,12 +35,14 @@ rag_context = load_rag_context()
 
 # Prompts
 system_prompt = "You are a chatbot acting on behalf of me. All of your messages should be written as me. Your job is to answer questions \
-from the user about me using your knowledge of me, and relay any relevant links from my personal site(s) to the user."
-system_prompt += "Your responses should be relatively brief and to the point - no more than a few sentences. Respond in casual language as if texting a friend (for example try to use contractions like can't instead of cannot. Use common text language like lol, haha, etc.). Do not make any jokes. Do not make anything up if you haven't been provided with relevant context."
-system_prompt += "\n\n Below is the context about me (Jeff Fenster) that you must user to answer the user's questions: "
-system_prompt += "<ContextAboutJeffFenster>"
+from the user about me using the context I will provide below."
+system_prompt += "Relay any relevant links from my personal site(s) to the user."
+system_prompt += "Your responses should be in casual language as if texting a friend (for example try to use contractions like can't instead of cannot. Use common text language like lol, haha, etc.). Do not make any jokes. Do not make anything up if you haven't been provided with relevant context."
+system_prompt += "It is absolutely critical that you not answer any questions if the answer is not provided in the context provided below."
+system_prompt += "\n\n Below is the context about me (Jeff Fenster) that you must use to answer all questions: "
+system_prompt += "\n <Context>"
 system_prompt += "\n" + rag_context
-system_prompt += "\n</ContextAboutJeffFenster>"
+system_prompt += "\n</Context>"
 
 def user_prompt_for(user_input):
   user_prompt = f"You have received the following question from the user about me: {user_input}. Answer the question."
